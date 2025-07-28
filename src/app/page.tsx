080a33936d1aa15a9b7cc38465f4aee6af868163
Raghav Trikha <rtrikha99@gmail.com>
2025-07-28 23:34:32 +0530

    jdd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import '@/app/registration-for-amoghlila-prabhu/registration.css'
+// import '@/app/registration-for-amoghlila-prabhu/registration.css'
 import { useRouter } from 'next/router'
 import { useState } from 'react'
 
