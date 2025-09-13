ac1797e07d64143dc383b2b0046372df65b10322
Raghav Trikha <rtrikha99@gmail.com>
2025-09-13 22:19:37 +0530

    UI fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -148,7 +148,7 @@ const Register_for_Event = () => {
                 <a href="https://chat.whatsapp.com/KpqLYmTYNZII1WfZvIKToh?mode=ems_copy_c"><Image width={50} src="/whatsapp.png" height={50}  alt='' className='fixed bottom-9 right-2.5' /><Image width={50} src="/whatsapp.png" height={50}  alt='' className='fixed bottom-9 right-2.5' /></a>
                 <div className='box-border'>
                     {/* Bannner */}
-                    <Image width={1000} src="/udaan.jpeg" height={100} className='w-[100vw] rounded-4xl p-4' alt='' />
+                    <Image width={1000} src="/udaan.jpeg" height={100} className='w-[100vw] rounded-4xl p-2' alt='' />
                     <div className="flex justify-center items-center min-h-screen" suppressHydrationWarning={true}>
                         <form action="" onSubmit={handleSubmit} className='flex justify-center items-center flex-col py-6 w-[90vw]'>
                             <h2 className="text-4xl">Register Now</h2>
