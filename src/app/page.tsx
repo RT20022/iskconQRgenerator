c2b413279c3438f1f6513421a0c497414119297c
Raghav Trikha <rtrikha99@gmail.com>
2025-10-05 08:22:16 +0530

    ::

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -168,8 +168,8 @@ const Register_for_Event = () => {
                 <div className='box-border'>
                     {/* Bannner */}
                     <Image width={1000} src="/udaan.png" height={100} className='w-[100vw] rounded-4xl p-2' alt='' />
-                    <div className={`flex justify-center items-center min-h-screen ${poppins.className}`} suppressHydrationWarning={true}>
-                        {!showForm && <div><h1 className='text-xl'>We are no longer accepting online registrations.</h1></div>}
+                    <div className={`flex justify-center items-center min-h-4 ${poppins.className}`} suppressHydrationWarning={true}>
+                        {!showForm && <div><h1 className='text-xl text-center'>We are no longer accepting online registrations.</h1></div>}
 
                         {showForm && <form action="" onSubmit={handleSubmit} className='flex justify-center items-center flex-col py-6 w-[90vw]'>
                             <h2 className="text-4xl">Register Now</h2>
